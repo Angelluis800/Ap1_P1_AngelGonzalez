af99ecd6d7faff8a01d9b7544268963a5178482c
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-06-03 18:38:18 -0400

    Agregando Contexto del Modelo

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Ap1_P1_AngelGonzalez.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ap1_P1_AngelGonzalez.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Articulos> Articulos { get; set; }
+}
