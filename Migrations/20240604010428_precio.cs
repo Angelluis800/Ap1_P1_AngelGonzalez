bfd63f021408121a5707b9a85081105092f6a31e
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-06-03 21:20:30 -0400

    Agregando Nueva Migracion en Donde Permita precios Nulos

---
+++ Migrations/20240604010428_precio.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ap1_P1_AngelGonzalez.Migrations
+{
+    /// <inheritdoc />
+    public partial class precio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
