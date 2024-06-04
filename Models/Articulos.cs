bd4694fdffaca5801a4b17d33effe33717bb0d87
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-06-03 21:23:22 -0400

    Permitiendo precio sea nulo

--- Models/Articulos.cs
+++ Models/Articulos.cs
@@ -20,7 +20,7 @@ public class Articulos
     [Range(0.01, 100, ErrorMessage = "El Rango de Ganancia debe tener un minimo de 0.01 hasta un 100%")]
     public decimal Ganancia { get; set; }
 
-    [Required(ErrorMessage = "Este Campo Debe Contener un Precio de Venta para el Artículo")]
-    [Range(0.01, 1000000000000, ErrorMessage = "El Costo debe estar entre el siguiente Rango 0.01 hasta 1,000,000,000,000")]
+    //[Required(ErrorMessage = "Este Campo Debe Contener un Precio de Venta para el Artículo")]
+    //[Range(0.01, 1000000000000, ErrorMessage = "El Costo debe estar entre el siguiente Rango 0.01 hasta 1,000,000,000,000")]
     public decimal Precio { get; set; }
 }
